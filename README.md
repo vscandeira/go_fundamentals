6c3a9574394104ea7562c4bb48a4975d0837ca80
Victor Candeira <vscandeira@gmail.com>
2025-02-25 13:13:05 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# go_fundamentals
+# GO - Fundamentals
+Basic repository created just to be a remote place for basic go codes while learning the fundamentals.
---
+++ strings.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	println(len("Hello, World!"))
+	println("Hello, World!"[0])
+	println(string("Hello, World!"[0]))
+	println("Hello" + ", World!")
+}
